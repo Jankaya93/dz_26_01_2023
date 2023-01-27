fe2395d0ba78788ee875142dbe5ca81ff15f2ba3
Oksana Oreshkina <jani93@mail.ru>
2023-01-27 21:45:52 +0300

    add dz 3 ot 26/01/2023

---
+++ dz_3_26_01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.Write("Введите число N: ");
+int cislo = Convert.ToInt32(Console.ReadLine());
+
+if(cislo%2 == 0)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+    System.Console.WriteLine("No");
