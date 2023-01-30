dd1f946cfd71d2abc63e2a045c5cc25df43dace0
Oksana Oreshkina <jani93@mail.ru>
2023-01-30 21:06:38 +0300

    add 30_01

---
+++ zadanie_1_sem_30_01/Program.cs
@@ -0,0 +1,20 @@
+﻿// написать программу, которая выводит случайно число из отрезка 10 - 99 и показывает наибольшую цифру числа.
+
+Random rand = new Random(); //чтоб покороче писать вывод рандома дальше по коду (если несколько раз нужно)
+int number = rand.Next(10, 100);
+
+System.Console.WriteLine($"Случайное число: {number}");
+
+
+if (number / 10 > number % 10)
+{
+    System.Console.WriteLine(number / 10);
+}
+else if (number / 10 < number % 10)
+{
+    System.Console.WriteLine(number % 10);
+}
+else
+{
+    System.Console.WriteLine("Цифры числа одинаковые");
+}
---
+++ zadanie_3_sem_30_01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+System.Console.Write("Введите число N1: ");
+int cislo_1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите число N2: ");
+int cislo_2 = Convert.ToInt32(Console.ReadLine());
+
+if (cislo_1 % cislo_2 == 0)
+{
+    System.Console.WriteLine("кратно");
+}
+else
+{
+    System.Console.WriteLine($"не кратно, остаток {cislo_1 % cislo_2}");
+}
