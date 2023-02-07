b829e220ae66db30e46b1a5339ae0e6b479dd417
Oksana Oreshkina <jani93@mail.ru>
2023-02-07 21:32:24 +0300

    add dz 06/02/2023-3

--- sem_4_dz_2/Program.cs
+++ sem_4_dz_2/Program.cs
@@ -3,34 +3,58 @@
 // 82 -> 10
 // 9012 -> 12
 
-int GetNumber(string text)
+
+// ===== Вариант 1
+// int GetNumber(string text)  // запрос исходного числа с текстом из вызова функции
+// {
+//     Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int NumberLen(int a) // считаем количество цифр в числе делением на 10
+// {
+//     int count = 0;
+//     while (a > 0)
+//     {
+//         a /= 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+// void SumNumbers(int n, int len) //подсчитываем сумму цифр числа
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= len; i++)
+//     {
+//         sum += n % 10;
+//         n /= 10;
+//     }
+//     Console.WriteLine(sum);
+// }
+
+// int number = GetNumber("Введите число: ");
+// int len = NumberLen(number);
+// SumNumbers(number, len);
+
+
+// ===== Вариант 2
+string? GetNumber(string text)  // запрос исходного числа с текстом из вызова функции
 {
     Console.Write(text);
-    return Convert.ToInt32(Console.ReadLine());
+    return Console.ReadLine();
 }
 
-int NumberLen(int a)
-{
-    int index = 0;
-    while (a > 0)
-    {
-        a /= 10;
-        index++;
-    }
-    return index;
-}
-
-void SumNumbers(int n, int len)
-{
+int SumNumbers(string number)  // сумма цифр числа с одновременным переводом из чар в инт.
+{   
     int sum = 0;
-    for (int i = 1; i <= len; i++)
+    for (int i = 0; i < number.Length; i++)
     {
-        sum += n % 10;
-        n /= 10;
+        sum = sum + Convert.ToInt32(Convert.ToString(number[i]));
     }
-    Console.WriteLine(sum);
+    return sum;
 }
 
-int number = GetNumber("Введите число: ");
-int len = NumberLen(number);
-SumNumbers(number, len);
+string? number = GetNumber("Введите число: ");
+System.Console.WriteLine(SumNumbers(number));
+
